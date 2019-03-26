c941460010ac218f0d6cb8ed68508df0ccb027a5
Roberto Brito <rbritoa@axtel.com.mx>
2019-03-25 18:16:16 -0600

    creamos logica para subirlo como bulk a elasticsearch

--- generaBulkBucketCorrespondiente.py
+++ generaBulkBucketCorrespondiente.py
@@ -1,6 +1,15 @@
 #Programado para la versión python3
 import argparse
 import json
+from elasticsearch import Elasticsearch
+from elasticsearch import helpers
+
+es = Elasticsearch(
+    ['localhost'],
+    port=9200
+)
+
+print(es)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--file", help="Ruta de archivo a procesar")
@@ -9,20 +18,31 @@ args = parser.parse_args()
  
 #if args.name is None: or args.file is None :
 if args.file is None :
-    print ("faltan parametros; tecleé --help o -h para ver las opciones")
+    print ("faltan parametros; --help o -h para ver las opciones")
     exit()
 if args.file:
-    # Abre archivo en modo lectura
-    archivoEntrada = open(args.file,'r')
-    # inicia bucle infinito para leer línea a línea
-    while True: 
-        linea = archivoEntrada.readline()  # lee línea
-        if not linea: 
-            break  # Si no hay más se rompe bucle
-        metadata = json.loads(linea)
-        tp = str(metadata["tp"])
-        create =  "{\"create\":{\"_index\":\"firdig_%s\",\"_type\":\"firdig\"}}" % (tp.lower())
-        print (create)
-        print(metadata)  # Muestra la línea leída
-    archivoEntrada.close  # Cierra archivoEntrada
+    
+    def getDataBulk():
+        # Abre archivo en modo lectura
+        archivoEntrada = open(args.file,'r')
+        # inicia bucle infinito para leer línea a línea
+        while True: 
+            linea = archivoEntrada.readline()  # lee línea
+            if not linea: 
+                break  # Si no hay más se rompe bucle
+            metadata = json.loads(linea)
+            tp = str(metadata["tp"])
+            # create =  "{\"create\":{\"_index\":\"firdig_%s\",\"_type\":\"firdig\"}}" % (tp.lower())
+            yield {
+                    "_index"  : "firdig_%s" % (tp.lower()),
+                    "_type"   : "firdig",
+                    "_source" : metadata
+            }
+            archivoEntrada.close  # Cierra archivoEntrada   
 
+    # mycont = getDataBulk()
+
+    # for i in mycont:
+    #     print(i)
+    helpers.bulk(es, getDataBulk())
+    
